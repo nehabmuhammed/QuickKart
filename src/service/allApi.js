beebb8a3378e9a4f14f4ffe02a53c329319e775e
nehabmuhammed <128158357+nehabmuhammed@users.noreply.github.com>
2025-11-18 14:25:17 +0530

    updated url

--- src/service/allApi.js
+++ src/service/allApi.js
@@ -1,28 +1,29 @@
 import axiosConfig from "./axiosConfig"
+import { baseUrl } from "./baseUrl"
 
 export const postData = async(reqBody) =>{
-    return await axiosConfig('post','http://localhost:3000/product',reqBody)
+    return await axiosConfig('post',`${baseUrl}/product`,reqBody)
 }
 
 
 export const loadData = async() =>{
-    return await axiosConfig('get','http://localhost:3000/product','')
+    return await axiosConfig('get',`${baseUrl}/product`,'')
 }
 export const deleteData = async(id) =>{
-    return await axiosConfig('delete',`http://localhost:3000/product/${id}`,{})
+    return await axiosConfig('delete',`${baseUrl}/product/${id}`,{})
 }
 export const editData= async(id,reqbody) =>{
-    return await axiosConfig('put',`http://localhost:3000/product/${id}`,reqbody)
+    return await axiosConfig('put',`${baseUrl}/product/${id}`,reqbody)
 
 }
 
 export const postDataCart = async(reqBody) =>{
-    return await axiosConfig('post','http://localhost:3000/cart',reqBody)
+    return await axiosConfig('post',`${baseUrl}/cart`,reqBody)
 }
 
 export const loadCartData = async() =>{
-    return await axiosConfig('get','http://localhost:3000/cart','')
+    return await axiosConfig('get',`${baseUrl}/cart`,'')
 }
 export const deleteCartData = async(id) =>{
-    return await axiosConfig('delete',`http://localhost:3000/cart/${id}`,{})
+    return await axiosConfig('delete',`${baseUrl}/cart/${id}`,{})
 }
